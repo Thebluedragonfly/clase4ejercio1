1fcffdb8fde5e9c41449cf278fed7a4782227c52
Maria Ignacia <thebluedragonfly@gmail.com>
2017-04-05 19:57:33 -0300

    commit1

---
+++ main.js
@@ -0,0 +1,14 @@
+//ingreso numeros
+var primerNumero = prompt ("ingrese primer numero")
+var segundoNumero = prompt ("ingrese otro numero")
+
+//resultados
+var suma =  Number(primerNumero)+Number(segundoNumero)
+var resta =  Number(primerNumero)-Number(segundoNumero)
+var mutiplicacion =  Number(primerNumero)*Number(segundoNumero)
+var dividir =  Number(primerNumero)/Number(segundoNumero)
+
+alerta("las suma de tus numeros es:" + suma)
+alerta("las suma de tus numeros es:" + resta)
+alerta("las suma de tus numeros es:" + multiplicacion)
+alerta("las suma de tus numeros es:" + dividir)
